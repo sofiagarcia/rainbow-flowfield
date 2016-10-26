d6c915ccc723206be7246ff0c6d632254780f178
Sofia Garcia <sofisagarcia@gmail.com>
2016-10-26 17:01:10 -0400

    basepoint

--- app/sketch.js
+++ app/sketch.js
@@ -3,26 +3,26 @@ var scl = 10;
 var cols, rows, canvas;
 var nameInput, button;
 
-
 var zoff = 0;
 
 var particles = [];
-
 var flowfield = [];
 
 function setup() {
   background(255);
   canvas = createCanvas(300, 300);
   canvas.parent(canvasContainer);
-
+  // drawing = new Drawing();
   greeting = createElement('h2', 'what is your name?');
 
   nameInput = createInput();
   button = createButton("Submit");
-  button.mousePressed(generateArt);
+  button.mousePressed(nameValue);
 
   cols = floor(width/scl);
   rows = floor(height/scl);
+
+  // drawing = new Drawing();
 
   flowfield = new Array(cols * rows);
 
@@ -31,26 +31,24 @@ function setup() {
   }
 
 }
+var letters = [];
+function nameValue(name){
+  this.name = nameInput.value();
+  // this.letters = [];
+  greeting.html('hello '+ this.name +'!');
+
+  for(var i = 0; i < this.name.length; i++){
+        letters.push(
+        this.name.charCodeAt(i)
+      );
+    }
+    console.log(letters);
+    createField();
+  }
 
 
-
-function generateArt() {
-      this.name = nameInput.value();
-      greeting.html('hello '+ this.name +'!');
-      this.letters = [];
-      // this.letters = name.split('');
-
-      for(var i = 0; i < this.name.length; i++){
-            this.letters.push(
-              this.name.charCodeAt(i)
-            );
-        //  );
-      }
-      console.log(this.letters);
-
-
-      this.yoff = 0;
-
+function createField(){
+    this.yoff = 0;
     for(var y = 0; y < rows; y++){
       this.xoff = 0;
       for(var x = 0; x < cols; x++) {
@@ -60,21 +58,22 @@ function generateArt() {
         this.v.setMag(1);
         flowfield[this.index] = this.v;
         this.xoff += inc;
-        stroke(0, 50);
-
+        stroke(50, 50);
       }
-      this.yoff += inc;
-      this.zoff += 0.001;
+    this.yoff += inc;
+    this.zoff += 0.001;
     }
-}
+  }
+
 
 function draw(){
-
   for(var i = 0; i < particles.length; i++){
     particles[i].follow(flowfield);
     particles[i].update();
     particles[i].edges();
     particles[i].show();
   }
+}
 
+function mousePressed() {
 }
